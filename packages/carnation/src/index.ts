2c7a27791b0d8181b97a8f65ddc2395ce4d2d66d
Josh Pensky <joshuapensky@gmail.com>
2023-04-04 01:16:23 -0400

    export types

--- packages/carnation/src/index.ts
+++ packages/carnation/src/index.ts
@@ -1,1 +1,10 @@
 export { c } from "./core";
+export { AccessibilityProvider, useAccessibility } from "./core/context";
+
+export type {
+  AccessibilityProps,
+  ElementProps,
+  PressableState,
+} from "./core/primitives/types";
+export type { TaggedTextProps } from "./core/primitives/Text/types";
+export type { TaggedViewProps } from "./core/primitives/View/types";
