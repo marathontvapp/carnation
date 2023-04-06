0cafb5493e32f636d0d8dcf900f2ccbe692d6888
Josh Pensky <joshuapensky@gmail.com>
2023-04-05 22:41:58 -0400

    feat: use Element name

--- packages/carnation/src/index.ts
+++ packages/carnation/src/index.ts
@@ -4,17 +4,17 @@ export type {
   ElementProps,
   PressableState,
 } from "./core/primitives/types";
-export type { TaggedTextProps as LayoutElementProps } from "./core/primitives/Text/types";
-export type { TaggedViewProps as TextElementProps } from "./core/primitives/View/types";
-export type { LinkProps } from "./core/primitives/Link/types";
-export type { ButtonProps } from "./core/primitives/Button/types";
-export type { ImageProps } from "./core/primitives/Image/types";
+export type { TaggedTextProps as TextElementProps } from "./core/primitives/Text/types";
+export type { TaggedViewProps as LayoutElementProps } from "./core/primitives/View/types";
+export type { LinkProps as LinkElementProps } from "./core/primitives/Link/types";
+export type { ButtonProps as ButtonElementProps } from "./core/primitives/Button/types";
+export type { ImageProps as ImageElementProps } from "./core/primitives/Image/types";
 export type {
-  SvgProps,
-  CircleProps,
-  LineProps,
-  PathProps,
-  RectProps,
+  SvgProps as SvgElementProps,
+  CircleProps as CircleElementProps,
+  LineProps as LineElementProps,
+  PathProps as PathElementProps,
+  RectProps as RectElementProps,
 } from "./core/primitives/Svg/types";
 
 export { c } from "./core";
