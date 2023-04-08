e2addd55630c2b9aafeb2206a55e84206b4c22dc
Josh Pensky <joshuapensky@gmail.com>
2023-04-08 09:42:16 -0400

    fix: reorder exports

--- packages/carnation/src/index.ts
+++ packages/carnation/src/index.ts
@@ -1,21 +1,18 @@
-export type { Config as CarnationConfig } from "./core/context";
+export { c, CarnationProvider, useAccessibility } from "./core";
+
 export type {
+  CarnationConfig,
   AccessibilityProps,
   ElementProps,
   PressableState,
-} from "./core/primitives/types";
-export type { TaggedTextProps as TextElementProps } from "./core/primitives/Text/types";
-export type { TaggedViewProps as LayoutElementProps } from "./core/primitives/View/types";
-export type { LinkProps as LinkElementProps } from "./core/primitives/Link/types";
-export type { ButtonProps as ButtonElementProps } from "./core/primitives/Button/types";
-export type { ImageProps as ImageElementProps } from "./core/primitives/Image/types";
-export type {
-  SvgProps as SvgElementProps,
-  CircleProps as CircleElementProps,
-  LineProps as LineElementProps,
-  PathProps as PathElementProps,
-  RectProps as RectElementProps,
-} from "./core/primitives/Svg/types";
-
-export { c } from "./core";
-export { CarnationProvider, useAccessibility } from "./core/context";
+  TextElementProps,
+  LayoutElementProps,
+  LinkElementProps,
+  ButtonElementProps,
+  ImageElementProps,
+  SvgElementProps,
+  CircleElementProps,
+  LineElementProps,
+  PathElementProps,
+  RectElementProps,
+} from "./core";
